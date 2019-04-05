87b0a5d60e1ef8f5f69226c10a29ead669ecb52e
Miguel Pazo <miguel.ps19@gmail.com>
2019-04-04 22:25:18 -0500

    fix acr

--- src/main/java/pe/gob/reniec/pki/idaas/sdk/enums/Acr.java
+++ src/main/java/pe/gob/reniec/pki/idaas/sdk/enums/Acr.java
@@ -17,7 +17,7 @@ public enum Acr {
     PKI_DNIE_MOBILE("pki_dnie_mobile"),
     FINGERPRINT_MOBILE("fingerprint_mobile"),
     PKI_DNIE_LEGACY("pki_dnie_legacy"),
-    PKI_TOKEN_LEGACY("pki_token_legacy ");
+    PKI_TOKEN_LEGACY("pki_token_legacy");
 
     private String value;
     private static final Map<String, Acr> lookup = new HashMap<>();
