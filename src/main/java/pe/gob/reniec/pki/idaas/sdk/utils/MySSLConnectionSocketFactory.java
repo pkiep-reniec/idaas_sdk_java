549bd55afd289face5138fd6c1ce495a361ff346
Miguel Pazo <miguel.ps19@gmail.com>
2019-05-27 11:06:14 -0500

    fix authors

--- src/main/java/pe/gob/reniec/pki/idaas/sdk/utils/MySSLConnectionSocketFactory.java
+++ src/main/java/pe/gob/reniec/pki/idaas/sdk/utils/MySSLConnectionSocketFactory.java
@@ -1,34 +1,37 @@
 package pe.gob.reniec.pki.idaas.sdk.utils;
 
-
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
+
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+/**
+ * @author Paulo Portugal
+ */
 public class MySSLConnectionSocketFactory {
 
-	public static SSLConnectionSocketFactory getConnectionSocketFactory() {
+    public static SSLConnectionSocketFactory getConnectionSocketFactory() {
 
-		SSLConnectionSocketFactory sslcf = null;
-		try {
-			SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(new TrustStrategy() {
-				@Override
-				public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-					return true;
-				}
-			}).build();
-			HostnameVerifier hnv = new NoopHostnameVerifier();
-			sslcf = new SSLConnectionSocketFactory(sslContext, hnv);
-		} catch (Exception e) {
-			sslcf = null;
-		}
+        SSLConnectionSocketFactory sslcf = null;
+        try {
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(new TrustStrategy() {
+                @Override
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            }).build();
+            HostnameVerifier hnv = new NoopHostnameVerifier();
+            sslcf = new SSLConnectionSocketFactory(sslContext, hnv);
+        } catch (Exception e) {
+            sslcf = null;
+        }
 
-		return sslcf;
-	}
+        return sslcf;
+    }
 
 }
